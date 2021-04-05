0b25640a92bb64bfe55c8449b6fd6378fd47881d
AmineDachir <68955063+aminedachir@users.noreply.github.com>
2021-04-05 23:38:46 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -10,17 +10,9 @@ def hom():
 def dv():
    return render_template('Division.html')
 
-@app.route("/summation")
-def home():
-   return render_template('summation.html')
-
 @app.route("/multiplication")
 def homes():
    return render_template('multiplication.html')
-
-@app.route("/Conversion")
-def js():
-   return render_template('js.html')
 
 if __name__ == '__main__':
    app.run(port=app.config['PORT'], debug=app.config['DEBUG'])
